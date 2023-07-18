5f58c4ef992d0f8b88921694487287cb1f1d8c52
Karssutin <lukas.vanhala@gmail.com>
2023-07-18 18:20:57 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Atom Tree",
+	id: "Atoms-tree",
+	author: "Karssutin",
+	pointsName: "Atoms",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
